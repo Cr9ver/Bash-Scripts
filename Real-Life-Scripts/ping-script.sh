e8c62d5c8d0b3654ffed2a3ee0699a6497154ad4
cr9ver <cr9vercloud@gmail.com>
2023-08-08 23:17:13 +0200

    added new scripts

--- Real-Life-Scripts/ping-script.sh
+++ Real-Life-Scripts/ping-script.sh
@@ -2,7 +2,7 @@
 # Description: Script to ping a remote host and notify 
 
 hosts="192.168.1.1"
-ping -c1 $hosts >> /dev/null
+ping -c1 $hosts &> /dev/null  # $> redirects both standard output and standard error
 if [ $? -eq 0 ]
 then
     echo $hosts OK
